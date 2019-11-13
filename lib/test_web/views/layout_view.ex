f8b34d0a8dfccb2cbf78850292817bcdb6fb01cd
Jlovelien <35080365+Jlovelien@users.noreply.github.com>
2019-11-13 02:37:18 -0500

    Clean up

--- lib/test_web/views/layout_view.ex
+++ lib/test_web/views/layout_view.ex
@@ -1,12 +1,12 @@
 defmodule TestWeb.LayoutView do
   use TestWeb, :view
-
+## Check if a user is logged in
   def logged_in?(conn) do
   	Guardian.Plug.authenticated?(conn, [])
   end
-
+## parse the username to be used for identification on page
   def username(conn) do
-    
+
     user = Guardian.Plug.current_resource(conn)
 
     %{username: username} = user
